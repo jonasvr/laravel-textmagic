4471dceb5caa1d4d80c33da4713e0b91af5f2813
Roland Kalocsaven <rolandka@live.com>
2016-02-10 15:18:16 +0000

    Initial Commit

---
+++ src/config/config.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+  /*
+  |--------------------------------------------------------------------------
+  | Textmagic Configuration
+  |--------------------------------------------------------------------------
+  | To start sending text messages using the TextMagic REST API, just follow these steps:
+  |
+  | 1. Generate the API credentials - https://my.textmagic.com/online/api/rest-api/keys
+  | 2. Connect to our API endpoint 
+  |
+  | To start sending text messages, you need to create an API key. API keys 
+  | are similar to an account password; the difference is that an API key 
+  | only provides access to the API: you cannot log in to TextMagic Online 
+  | using the API key.
+  |
+  | Your program sends the login credentials with each API request as HTTP headers: X-TM-Username is your TextMagic username, while X-TM-Key is your API key.
+  |
+  | Please configure your username and token below.
+  |
+  */
+
+  'username' => '',
+  'token' => '',
+  'version' => null,
+  'http' => null,
+);
